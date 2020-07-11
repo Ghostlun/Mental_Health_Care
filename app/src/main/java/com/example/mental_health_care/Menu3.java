7e628011676ccdddb8805a99a391a34ab2b1cc3a
Ghostlun <40644178+Ghostlun@users.noreply.github.com>
2020-07-11 14:37:34 -0500

    update
    update

--- app/src/main/java/com/example/mental_health_care/Menu3.java
+++ app/src/main/java/com/example/mental_health_care/Menu3.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,24 +14,31 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Menu3 extends AppCompatActivity {
 
 
-    Integer level = 1;
+   private int level = 1;
+   TextView questionView;
+    Button button1;
+    Button button2;
+    Button button3;
 
-    public Integer getLevel(){
+
+    public int getLevel(){
 
         return level;
     }
 
-    public void setLevel(int Level){
+    public void setLevel(int level){
 
-        this.level = Level;
+        this.level = level;
     }
 
-    public Integer levelup(){
+    public int levelup(){
 
         level = level + 1;
 
         return level;
     }
+
+
 
 
     @Override
@@ -38,68 +47,94 @@ public class Menu3 extends AppCompatActivity {
         setContentView(R.layout.menu3);
 
 
-        TextView questionView =  (TextView) findViewById(R.id.questionBoard);
-        Button button1 = (Button) findViewById(R.id.button1);
-        Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-
-//        TextView counter = (TextView) findViewById(R.id.counter);
-//        counter.setText(getLevel());
-
-        button1.setText(getLevel().toString());
-        questionView.setText("How are you doing Today?");
-
-
-        switch(level){
-
-
-            case 1 :
-                questionView.setText("My name is Joy, How are you doing Today? \n Choose the one of the emotion words below");
-                button1.setText("I am tired");
-                button2.setText("I am despressed");
-                button3.setText("I am sad");
-                break;
-
-            case 2 :
-                questionView.setText("It's important to know how i feel, can you tell me what happened at that time");
-
-
-                break;
-
-            case 3 : questionView.setText("I am always with you \n please don't hesitate contact with me" );
+        questionView =  (TextView) findViewById(R.id.questionBoard);
+         button1 = (Button) findViewById(R.id.button1);
+         button2 = (Button) findViewById(R.id.button2);
+         button3 = (Button) findViewById(R.id.button3);
+        EditText Edittext1 = (EditText) findViewById(R.id.textBox);
 
 
 
-            break;
 
 
 
-            default: questionView.setText("I am sorry, can you give me detail information");
 
-        }
 
 
         button1.setOnClickListener(new View.OnClickListener() {
+
+            //            Level은 올라간다.
             @Override
             public void onClick(View v) {
-                levelup();
-        }
+                level++;
+                setLevel(level);
+                Toast.makeText(getApplicationContext(),Integer.toString(getLevel()) ,Toast.LENGTH_SHORT).show();
+                changeQeustion(level);
+            }
         });
 
         button2.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
 
-            levelup();        }
-         });
+                level++;
+                setLevel(level);
+                changeQeustion(level);
+
+
+            }
+        });
 
 
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                level++;
+                setLevel(level);
+                changeQeustion(level);
 
-                levelup();            }
+            }
         });
+
+
+//       First Question
+        questionView.setText("My name is Joy, How are you doing Today? \n Choose the one of the emotion words below");
+        button1.setText("I don't have power today");
+        button2.setText("I am despressed");
+        button3.setText("I am sad");
+    }
+
+    public void changeQeustion(int update) {
+
+        switch(update){
+
+
+            case 1 :
+
+                questionView.setText("My name is Joy, How are you doing Today? \n Choose the one of the emotion words below");
+
+                break;
+
+            case 2 :
+                questionView.setText("It's important to know how i feel, can you tell me what happened at that time");
+                button1.setVisibility(View.GONE);
+                button1.setText("I argued with my friend");
+                button2.setText("I got a bad grade");
+                button3.setText("I am tired");
+
+                break;
+
+            case 3 : questionView.setText("I am always with you \n please don't hesitate contact with me" );
+                button1.setText("I need help");
+                button2.setText("Thank you");
+                button3.setVisibility(View.GONE);
+                break;
+
+
+
+            default: questionView.setText("Thank you for helping me, \n I am so glad to help you");
+
+        }
 
     }
 
